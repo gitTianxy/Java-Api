d60c9dc54c5459f427a5c0c00d0f57654e44e471
kevintian <kevintian@pptv.com>
2017-10-09 16:43:23 +0800

    add: http request with proxy

--- src/main/java/com/java/api/apache/http/HttpClientDemo.java
+++ src/main/java/com/java/api/apache/http/HttpClientDemo.java
@@ -3,14 +3,20 @@ package com.java.api.apache.http;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.ParseException;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
@@ -26,7 +32,6 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -38,21 +43,6 @@ import java.util.Map;
  * Created by kevintian on 2017/9/29.
  */
 public class HttpClientDemo {
-    public static void main(String[] args) {
-        HttpClientDemo demo = new HttpClientDemo();
-        // test get
-//        demo.get("https://www.baidu.com/");
-        // test post
-        String postUrl = "http://localhost:8088/rest/jpa_entity";
-        Map<String, String> postHeaders = new HashMap<>();
-        postHeaders.put("Content-Type", "application/json");
-        Map<String, Object> postBody = new HashMap<>();
-        long ts = System.currentTimeMillis();
-        postBody.put("fieldA", "fA" + ts);
-        postBody.put("fieldB", "fB" + ts);
-        demo.post(postUrl, postHeaders, postBody);
-    }
-
     /**
      * 发送 get请求
      */
@@ -95,6 +85,37 @@ public class HttpClientDemo {
             }
         }
     }
+
+    public void getWithProxy(String uri, Proxy proxyParams) {
+        // set proxy
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        if (proxyParams.getUserName() != null && proxyParams.getPassword() != null) {
+            credsProvider.setCredentials(
+                    new AuthScope(proxyParams.hostName, proxyParams.port),
+                    new UsernamePasswordCredentials(proxyParams.userName, proxyParams.password));
+        }
+        HttpHost proxy = new HttpHost(proxyParams.hostName, proxyParams.port);
+        RequestConfig config = RequestConfig.custom()
+                .setProxy(proxy)
+                .build();
+        HttpGet request = new HttpGet(uri);
+        request.setConfig(config);
+        try (CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultCredentialsProvider(credsProvider).build()) {
+            System.out.println("Executing request " + request.getRequestLine());
+            CloseableHttpResponse response = httpClient.execute(request);
+            try {
+                System.out.println(response.getStatusLine());
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } finally {
+                response.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     /**
      * 发送 post请求访问本地应用并根据传递参数不同返回不同结果
@@ -139,6 +160,53 @@ public class HttpClientDemo {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    public void postWithProxy(String uri, Map<String, String> headers, Map<String, Object> body, Proxy proxyParams) {
+        HttpPost httppost = new HttpPost(uri);
+        // add headers
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            httppost.setHeader(entry.getKey(), entry.getValue());
+        }
+        // set body
+        try {
+            StringEntity json = new StringEntity(new ObjectMapper().writeValueAsString(body), "utf-8");
+            httppost.setEntity(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        // set proxy
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        if (proxyParams.getUserName() != null && proxyParams.getPassword() != null) {
+            credsProvider.setCredentials(
+                    new AuthScope(proxyParams.hostName, proxyParams.port),
+                    new UsernamePasswordCredentials(proxyParams.userName, proxyParams.password));
+        }
+        HttpHost proxy = new HttpHost(proxyParams.hostName, proxyParams.port);
+        RequestConfig config = RequestConfig.custom()
+                .setProxy(proxy)
+                .build();
+        httppost.setConfig(config);
+        try (CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build()) {
+            System.out.println("POST request " + httppost.getURI());
+            CloseableHttpResponse response = httpClient.execute(httppost);
+            try {
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    System.out.println("--------------------------------------");
+                    System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));
+                    System.out.println("--------------------------------------");
+                }
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -201,6 +269,57 @@ public class HttpClientDemo {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    class Proxy {
+        String hostName;
+        Integer port;
+        String userName;
+        String password;
+
+        public Proxy(String hostName, Integer port) {
+            this.hostName = hostName;
+            this.port = port;
+        }
+
+        public Proxy(String hostName, Integer port, String userName, String password) {
+            this.hostName = hostName;
+            this.port = port;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public String getHostName() {
+            return hostName;
+        }
+
+        public void setHostName(String hostName) {
+            this.hostName = hostName;
+        }
+
+        public Integer getPort() {
+            return port;
+        }
+
+        public void setPort(Integer port) {
+            this.port = port;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
         }
     }
 }
