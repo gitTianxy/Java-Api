0927c2120bed4ff5e216797336c6ba4f7c9195f6
gitTianxy <txy_learning@163.com>
2017-10-09 11:15:24 +0800

    add: pip io demo

--- src/main/java/com/java/api/io/NewIODemo.java
+++ src/main/java/com/java/api/io/NewIODemo.java
@@ -1,51 +1,146 @@
 package com.java.api.io;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.Pipe;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.charset.Charset;
 import java.nio.file.*;
-import java.util.*;
+import java.util.Iterator;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
+ * CORE MEMBERS:
+ * 1. Buffers: ByteBuffer, CharBuffer, other data-buffers
+ * 2. Channels: FileChannel, SocketChannel/ServerSocketChannel, DatagramChannel
+ * 3. Selectors: [relations] one selector with many channels, one channel with one or more buffer
+ * <p>
+ * UTILITIES:
+ * 1. Files
+ * 2. Paths
+ * <p>
  * Created by kevintian on 2017/9/27.
  */
-public class NativeIODemo {
+public class NewIODemo {
     final String FILE_ROOT = System.getProperty("user.home") + "/Desktop/io_test/";
 
-    public static void main(String[] args) throws Exception {
-        NativeIODemo demo = new NativeIODemo();
-        demo.ioApi();
-        demo.nioApi();
-    }
-
     /**
-     * CORE MEMBERS:
-     * 1. Buffers: ByteBuffer, CharBuffer, other data-buffers
-     * 2. Channels: FileChannel, SocketChannel/ServerSocketChannel, DatagramChannel
-     * 3. Selectors: [relations] one selector with many channels, one channel with one or more buffer
-     * <p>
-     * UTILITIES:
-     * 1. Files
-     * 2. Paths
+     * DEMOS
+     * 1. FileChannel
+     * 2. SocketChannel(client),ServerSocketChannel(server)
+     * 3. Files
+     * 4. Pipe
+     *
+     * @param args
+     * @throws Exception
      */
-    void nioApi() throws Exception {
-        channelDemo();
-        bufferDemo();
-        selectorDemo();
-        filesDemo();
+    public static void main(String[] args) throws Exception {
+        NewIODemo demo = new NewIODemo();
+        /*demo.fileChannelDemo();
+        demo.socketChannelDemo();
+        demo.filesDemo();*/
+        demo.pipeDemo();
     }
 
     /**
-     * CONTENT: 1-7
+     * Java NIO 管道是'2个'线程之间的单向数据连接。Pipe有一个source通道和一个sink通道。
+     */
+    void pipeDemo() throws IOException {
+        final Pipe pipe = Pipe.open();
+        Selector selector = Selector.open();
+
+
+        Runnable senderTsk = new Runnable() {
+            @Override
+            public void run() {
+                try (Pipe.SinkChannel sinkChannel = pipe.sink()) {
+                    while (true) {
+                        String msg = "send: " + System.currentTimeMillis();
+                        System.out.println(msg);
+                        ByteBuffer buf = ByteBuffer.allocate(48);
+                        buf.clear();
+                        buf.put(msg.getBytes());
+                        buf.flip();
+                        while (buf.hasRemaining()) {
+                            sinkChannel.write(buf);
+                        }
+                        Thread.sleep(1000);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable accepterTsk1 = new Runnable() {
+
+            @Override
+            public void run() {
+                try (Pipe.SourceChannel sourceChannel = pipe.source()) {
+                    int opts = sourceChannel.validOps();
+                    sourceChannel.register(selector, opts);
+                    while (true) {
+                        System.out.println("waiting for msg.");
+                        selector.select();
+                        Iterator<SelectionKey> itr = selector.selectedKeys().iterator();
+                        while (itr.hasNext()) {
+                            SelectionKey sKey = itr.next();
+                            if (sKey.isReadable()) {
+
+                                Pipe.SourceChannel channel = (Pipe.SourceChannel) sKey.channel();
+                                ByteBuffer buf = ByteBuffer.allocate(48);
+                                int bytesRead = channel.read(buf);
+                                while (bytesRead != -1) {
+                                    buf.flip();
+                                    System.out.println("receive 1: " + new String(buf.array(), 0, bytesRead, "utf-8"));
+                                    buf.clear();
+                                    bytesRead = sourceChannel.read(buf);
+                                }
+                            }
+                            itr.remove();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable accepterTsk2 = new Runnable() {
+            @Override
+            public void run() {
+                try (Pipe.SourceChannel sourceChannel = pipe.source()) {
+                    ByteBuffer buf = ByteBuffer.allocate(48);
+                    int bytesRead = sourceChannel.read(buf);
+                    while (bytesRead != -1) {
+                        buf.flip();
+                        System.out.println("receive 2: " + new String(buf.array(), 0, bytesRead, "utf-8"));
+                        buf.clear();
+                        bytesRead = sourceChannel.read(buf);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Executor executor = Executors.newFixedThreadPool(3);
+        executor.execute(senderTsk);
+        executor.execute(accepterTsk1);
+//        executor.execute(accepterTsk2);
+
+    }
+
+    /**
+     * Operations: 1-7
      */
     void filesDemo() throws IOException {
         Path path = Paths.get(FILE_ROOT, "test.txt");
@@ -60,7 +155,7 @@ public class NativeIODemo {
         }
         // 2. write
         try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-            for (int i=0; i<100; i++) {
+            for (int i = 0; i < 100; i++) {
                 writer.write(String.format("第%s行: %s\n", i, String.valueOf(Math.random())));
             }
             writer.flush();
@@ -89,7 +184,7 @@ public class NativeIODemo {
             }
         });
         Path tmp = Files.createTempFile(path.getParent(), "tmp", ".txt");
-        try(PrintWriter printer = new PrintWriter(Files.newOutputStream(tmp))){
+        try (PrintWriter printer = new PrintWriter(Files.newOutputStream(tmp))) {
             printer.write("this is a tempt file written by 'PrintWriter'");
             printer.flush();
         }
@@ -100,11 +195,12 @@ public class NativeIODemo {
     /**
      * METHODS:
      * 1. read(buffer): write data into buffer(s)
-     *      当写入buffer数组时, 按数组元素顺序依次写入, 一个写满写下一个
+     * 当写入buffer数组时, 按数组元素顺序依次写入, 一个写满写下一个
      * 2. write(buffer): read data from buffer(s)
-     *      当从buffer数组读取时, 按数组元素顺序依次读入, 一个读完读下一个
+     * 当从buffer数组读取时, 按数组元素顺序依次读入, 一个读完读下一个
      * 3. close(): close channel after use
      * UTILITIES: Channels
+     *
      * @throws IOException
      */
     private void channelDemo() throws IOException, InterruptedException {
@@ -113,21 +209,21 @@ public class NativeIODemo {
     }
 
     /**
-     *
      * @throws IOException
      */
     private void socketChannelDemo() throws IOException, InterruptedException {
-        // refer to 'socketIO()'
+        socketIO();
     }
 
     /**
      * FileChannel
-     *      1) transferFrom(): read data from other channel
-     *      2) transferTo(): write data to other channel
-     *      3) postion(): operate position in the related file
-     *      4) size(): size of the related file
-     *      5) lock(): get 'FileLock' as multiple program trying to operate on a same file
+     * 1) transferFrom(): read data from other channel
+     * 2) transferTo(): write data to other channel
+     * 3) postion(): operate position in the related file
+     * 4) size(): size of the related file
+     * 5) lock(): get 'FileLock' as multiple program trying to operate on a same file
      * CONSTRUCTION: from InputStream, OutputStream, RandomAccessFile
+     *
      * @throws IOException
      */
     private void fileChannelDemo() throws IOException {
@@ -162,7 +258,7 @@ public class NativeIODemo {
         buf1.clear();
         buf1.put("New String to write to file...".getBytes());
         buf1.flip();
-        while(buf1.hasRemaining()) {
+        while (buf1.hasRemaining()) {
             outChannel.write(buf1);
         }
         long fileSize = outChannel.size();
@@ -182,7 +278,7 @@ public class NativeIODemo {
      * 缓冲区本质上是一块供读/写数据的内存。这块内存被包装成NIO Buffer对象，并提供了一组方法，用来方便的访问该块内存。
      * PROPERTIES:
      * 1. capacity: 作为一个内存块，Buffer有一个固定的大小值，也叫“capacity”, 你只能往里写capacity个byte、long，char等类型。
-     *      一旦Buffer满了，需要将其清空（通过读数据或者清除数据）才能继续写数据往里写数据。
+     * 一旦Buffer满了，需要将其清空（通过读数据或者清除数据）才能继续写数据往里写数据。
      * 2. limit: 读的limit=[0, position], 写的limit=capacity
      * 3. position: 在buffer中读/写的当前位置; 完成一次操作, position移动到下一个读/写位置; position_max=capacity-1
      * METHODS:
@@ -201,14 +297,14 @@ public class NativeIODemo {
      * METHODS:
      * 1. open()/close(): create/destroy a selector
      * 2. SelectionKey: 每次向selector注册一个channel会返回一个SelectionKey, 之后可以用它来获取channel相关的信息, 也可以继续在它上面附加信息
-     *      1) key.interestOps(): Retrieves this key's interest set
-     *      2) key.selector(): Returns the selector for which this key was created
-     *      3) key.channel(): Returns the channel for which this key was created
-     *      4) key.attach(obj): attach data to channel
-     *      5) selector.selectedKeys(): Returns a set of SelectionKey
+     * 1) key.interestOps(): Retrieves this key's interest set
+     * 2) key.selector(): Returns the selector for which this key was created
+     * 3) key.channel(): Returns the channel for which this key was created
+     * 4) key.attach(obj): attach data to channel
+     * 5) selector.selectedKeys(): Returns a set of SelectionKey
      * 3. selectedKeys(): Returns all the SelectionKey related to a selector
      * 4. select(): 返回的int值表示有多少通道已经就绪。
-     *
+     * <p>
      * NOTE:
      * 1. 只有非阻塞的channel能和selector结合使用 (selectable channel: SocketChannel, ServerSocketChannel), 而FileChannel无非阻塞模式.
      * 2. Selector使用完关闭: close()
@@ -240,86 +336,6 @@ public class NativeIODemo {
             }
         }
         */
-    }
-
-    /**
-     * INPUT(3 LEVELS):
-     * 1. InputStream
-     * 2. Reader
-     * 3. children class of Reader, e.g. BufferedReader
-     *
-     * OUTPUT(3 LEVELS):
-     * 1. OutputStream
-     * 2. Writer
-     * 3. children class of Writer, e.g. BufferedWriter, PrintWriter(more powerful)
-     *
-     * @throws IOException
-     */
-    void ioApi() throws Exception {
-        // refer to 'httpIO()'
-    }
-
-    /**
-     * CONTENT:
-     * 1. input(do Get)
-     * 2. output(do POST)
-     *
-     * @throws IOException
-     */
-    void httpIO() throws IOException {
-        URLInput();
-        URLOutput();
-    }
-
-    /**
-     * do GET: use the basic URLConnection
-     *
-     * @throws IOException
-     */
-    private void URLInput() throws IOException {
-        URL url = new URL("https://www.baidu.com");
-        URLConnection connection = url.openConnection();
-        InputStream in = connection.getInputStream();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-            String content = reader.readLine();
-            while (content != null) {
-                System.out.println(content);
-                content = reader.readLine();
-            }
-        }
-        Map<String, List<String>> headers = connection.getHeaderFields();
-        for (Map.Entry<String, List<String>> hd : headers.entrySet()) {
-            System.out.println(String.format("head: key=%s, value=%s", hd.getKey(), hd.getValue()));
-        }
-    }
-
-    /**
-     * do POST: use HttpURLConnection
-     *
-     * @throws IOException
-     */
-    private void URLOutput() throws IOException {
-        URL target = new URL("http://localhost:8088/rest/jpa_entity");
-        HttpURLConnection httpConn = (HttpURLConnection) target.openConnection();
-        httpConn.setDoOutput(true);
-        httpConn.setRequestMethod("POST");
-        httpConn.setRequestProperty("Content-Type", "application/json");
-        httpConn.setRequestProperty("Accept-Charset", "utf-8");
-        Map<String, Object> bdParams = new HashMap<>();
-        bdParams.put("fieldA", "urirest-test fA");
-        bdParams.put("fieldB", "urirest-test fB");
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(httpConn.getOutputStream()))) {
-            writer.write(new ObjectMapper().writeValueAsString(bdParams));
-            writer.flush();
-        }
-        System.out.println("status: "+ httpConn.getResponseCode());
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(httpConn.getInputStream()))) {
-            String response = reader.readLine();
-            while (response != null) {
-                System.out.println(String.format(response));
-                response = reader.readLine();
-            }
-        }
     }
 
     /**
